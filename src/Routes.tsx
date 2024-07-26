30705e22b938a9bb80716086fc4d0c273079e2fe
dohye1 <tsc03018@gmail.com>
2024-07-26 23:12:41 +0900

    :sparkles: Lint, prettier, router, tailwind 설정

---
+++ src/Routes.tsx
@@ -0,0 +1,14 @@
+﻿import { createBrowserRouter } from 'react-router-dom';
+import Playground from './pages/Playground';
+import Duck from './pages/Duck';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Playground />,
+  },
+  {
+    path: '/duck',
+    element: <Duck />,
+  },
+]);
---
+++ src/pages/Duck.tsx
@@ -0,0 +1,5 @@
+﻿import React from 'react';
+
+export default function Duck() {
+  return <div>Duck</div>;
+}
